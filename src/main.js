d5e0e67c721a74d627c73a7cf020bb7fda6bc835
samudraam <112435495+samudraam@users.noreply.github.com>
2025-04-02 04:19:17 -0500

    updating paths

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ const boxes = document.querySelectorAll(".number-box");
 boxes.forEach((box, index) => {
   box.addEventListener("click", () => {
     const id = index + 1;
-    window.location.assign(`/viewer.html?id=${id}`);
+    window.location.assign(`/char3/viewer.html?id=${id}`);
   });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/char3/", // << important
   build: {
     rollupOptions: {
       input: {
